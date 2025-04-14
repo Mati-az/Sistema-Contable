48c635a3b4b4abbd725f8af11250cd20b22c46d2
Mati-az <matiaz.chevez.c@uni.pe>
2025-04-13 23:02:48 -0500

    lago

--- pages/05_📒_Saldos_de_Cuentas.py
+++ pages/05_📒_Saldos_de_Cuentas.py
@@ -5,6 +5,49 @@ st.set_page_config(page_title="Saldos de Cuentas", layout="wide")
 
 st.title("📒 Saldos de las Cuentas")
 
+st.markdown(
+    """
+    <style>
+
+        section[data-testid="stSidebar"] {
+            background-color: #dbeafe;
+        }
+
+        [data-testid="stSidebarNav"] a {
+            color: #111827 !important;
+        }
+
+        [data-testid="stSidebarNav"] a:hover,
+        [data-testid="stSidebarNav"] a:focus,
+        [data-testid="stSidebarNav"] a:active,
+        [data-testid="stSidebarNav"] a[aria-current="page"] {
+            background-color: #93c5fd !important;
+            color: #111827 !important;
+            font-weight: bold;
+            border-radius: 10px;
+            margin: 0px 25px;
+            padding: 0px 5px;
+            
+            display: flex;
+            align-items: center;
+            gap: 10px; /* espacio entre ícono y texto */
+            text-decoration: none;
+        }
+
+        [data-testid="stSidebarNav"]::before {
+            content: "🖥️ Dashboard";
+            display: block;
+            margin-left: 20px;
+            margin-bottom: 15px;
+            font-size: 30px;
+            font-weight: 700;
+            color: #111827;
+        }
+    </style>
+    """,
+    unsafe_allow_html=True,
+)
+
 # Obtener datos
 saldos_df = obtener_saldos_cuentas()
 
